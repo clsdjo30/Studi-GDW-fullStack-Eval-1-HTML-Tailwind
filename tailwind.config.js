fc15b79eb03309ec19108698d21c596c85b40cf7
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-03-07 13:07:47 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily : {
+      'sans': ['Segoe UI', 'Helvetica', 'sans-serif'],
+      'body': ['Poppins']
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
